d8bc6a87ffb7195da8cef5502f719cdb6c70da7b
benshidanny11 <benshidanny11@gmail.com>
2022-11-24 23:51:22 +0200

    Implement hello rails back end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Hello my friend")
+Greeting.create(message: "Hello my brother")
+Greeting.create(message: "Hello Danny")
+Greeting.create(message: "Hello Daniel")
+Greeting.create(message: "Hello Christne")
